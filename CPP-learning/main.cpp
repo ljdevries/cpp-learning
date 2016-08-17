069239429a9ff69269bcd3b67350fb77980715fa
Luke de Vries <luke@MacBook-Pro.local>
2016-08-17 22:47:01 +0800

    Initial Commit

---
+++ CPP-learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-learning
+//
+//  Created by Luke de Vries on 17/08/2016.
+//  Copyright (c) 2016 Luke de Vries. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
